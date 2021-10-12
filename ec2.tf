055230b8b3e90b6d3522bcae1cb83afdb99235fb
root <root@p-hdk6ihz6eoho-workstation.c.instruqt-prod.internal>
2021-10-12 02:49:23 +0000

    add tags to comply with policy.  Policy 1: iac-lab-first-policy/enforce tags (hard-mandatory)
    Result: false
    
    Description:
      This policy uses the Sentinel tfplan import to require that all EC2 instances  have all mandatory tags.

--- ec2.tf
+++ ec2.tf
@@ -2,10 +2,10 @@ resource "aws_instance" "tfc-mgarcia16-ec2" {
   instance_type     = "t3.nano"
   ami               = "ami-06114735d23400102" #AWS Linux
   availability_zone = "us-west-2c"
-  #  tags = {
-  #    Name        = "tfc-mgarcia16-ec2"
-  #    Environment = "Sandbox"
-  #  }
+  tags = {
+    Name        = "tfc-mgarcia16-ec2"
+    Environment = "Sandbox"
+  }
 
   root_block_device {
     volume_size           = "40"
