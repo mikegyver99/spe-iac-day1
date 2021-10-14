fe8a8ccbda9bad4a8c69aadb988ad5ae2bfd8b18
root <root@p-ijk7rois2y03-workstation.c.instruqt-prod.internal>
2021-10-14 20:22:23 +0000

    remove block device, fmt

--- ec2.tf
+++ ec2.tf
@@ -6,7 +6,7 @@ data "aws_ami" "ubuntu" {
     #values = ["ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"]
     values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
   }
-  
+
   filter {
     name   = "virtualization-type"
     values = ["hvm"]
@@ -15,19 +15,12 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
-  resource "aws_instance" "tfc-mgarcia16-ec2" {
-    instance_type     = "t3.nano"
-    ami               = data.aws_ami.ubuntu.id
-    availability_zone = "us-west-2c"
-    tags = {
-      Name        = "tfc-mgarcia16-ec2"
-      Environment = "Sandbox"
-    }
-  }
-
-  root_block_device {
-    volume_size           = "40"
-    volume_type           = "gp2"
-    delete_on_termination = "true"
+resource "aws_instance" "tfc-mgarcia16-ec2" {
+  instance_type     = "t3.nano"
+  ami               = data.aws_ami.ubuntu.id
+  availability_zone = "us-west-2c"
+  tags = {
+    Name        = "tfc-mgarcia16-ec2"
+    Environment = "Sandbox"
   }
 }
