6a1d21cf1b2ec7c8f76ce634b2963b4d628fedc1
MikeGyver <mikejgarcia@gmail.com>
2021-10-14 13:08:09 -0700

    Update ec2.tf
    remove ami entry

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,5 @@
 resource "aws_instance" "tfc-mgarcia16-ec2" {
   instance_type     = "t3.nano"
-  ami               = "ami-0c2d06d50ce30b442" #AWS Linux
   availability_zone = "us-west-2c"
   tags = {
     Name        = "tfc-mgarcia16-ec2"
