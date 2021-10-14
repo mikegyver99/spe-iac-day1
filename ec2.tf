d201037313ea3237c05ba867586700a0336430b0
root <root@p-ijk7rois2y03-workstation.c.instruqt-prod.internal>
2021-10-14 23:19:40 +0000

    remove tags rsa, An argument named tags is not expected here.

--- ec2.tf
+++ ec2.tf
@@ -69,11 +69,6 @@ resource "aws_instance" "hashicat" {
 resource "null_resource" "configure-cat-app" {
   depends_on = [aws_eip_association.hashicat]
 
-  tags = {
-    Name        = "${var.prefix}-ec2"
-    Environment = "Sandbox"
-  }
-
   triggers = {
     build_number = timestamp()
   }
