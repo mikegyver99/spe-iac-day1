2132c68eae7bdadf27c2246cac49007a6ecd8d76
MikeGyver <mikejgarcia@gmail.com>
2021-10-14 13:12:13 -0700

    add data var for ami
    data.aws_ami.ubuntu.id

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,15 @@
-resource "aws_instance" "tfc-mgarcia16-ec2" {
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    #values = ["ubuntu/images/hvm-ssd/ubuntu-disco-19.04-amd64-server-*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-bionic-18.04-amd64-server-*"]
+  }
+
+  resource "aws_instance" "tfc-mgarcia16-ec2" {
   instance_type     = "t3.nano"
+  ami = data.aws_ami.ubuntu.id
   availability_zone = "us-west-2c"
   tags = {
     Name        = "tfc-mgarcia16-ec2"
